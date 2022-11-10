f4ee955bb69fd01124dd6ecfc861a34cce2b2fc9
fatima <fatimagull863@gmail.com>
2022-11-10 16:01:28 +0500

    SCLWLK-26 Admin Panel Completed

---
+++ ISocial/include/footerscripts.php
@@ -0,0 +1,16 @@
+   <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+   <script src="assets/libs/metismenujs/metismenujs.min.js"></script>
+   <script src="assets/libs/simplebar/simplebar.min.js"></script>
+   <script src="assets/libs/eva-icons/eva.min.js"></script>
+
+
+   <script src="assets/libs/apexcharts/apexcharts.min.js"></script>
+   <script src="assets/libs/flatpickr/flatpickr.min.js"></script>
+
+   <script src="assets/libs/jsvectormap/js/jsvectormap.min.js"></script>
+   <script src="assets/libs/jsvectormap/maps/world-merc.js"></script>
+   <script src="assets/js/pages/pass-addon.init.js"></script>
+   <script src="assets/js/pages/dashboard.init.js"></script>
+   <script src="assets/js/pages/todo.init.js"></script>
+
+   <script src="assets/js/app.js"></script>
---
+++ ISocial/include/sidebar.php
@@ -0,0 +1,51 @@
+<div id="sidebar-menu">
+    <!-- Left Menu Start -->
+    <ul class="metismenu list-unstyled" id="side-menu">
+        <li class="menu-title" data-key="t-menu">Dashboard</li>
+
+        <li>
+            <a href="javascript: void(0);">
+                <i class="bx bx-home-alt icon nav-icon"></i>
+                <span class="menu-item" data-key="t-dashboard">Dashboard</span>
+                <span class="badge rounded-pill bg-primary">2</span>
+            </a>
+            <ul class="sub-menu" aria-expanded="false">
+                <li><a href="index.php" data-key="t-ecommerce">Dashboard</a></li>
+
+            </ul>
+        </li>
+
+        <li class="menu-title" data-key="t-applications">Applications</li>
+
+
+        <li>
+            <a href="index.php">
+                <i class="bx bx-user-pin icon nav-icon"></i>
+                <span class="menu-item" data-key="t-calendar">Users</span>
+            </a>
+        </li>
+
+        <li>
+            <a href="Groups.php">
+                <i class="bx bx-check-square icon nav-icon"></i>
+                <span class="menu-item" data-key="t-todo">Groups</span>
+
+            </a>
+        </li>
+        <li>
+            <a href="challenges.php">
+                <i class="bx bx-check-square icon nav-icon"></i>
+                <span class="menu-item" data-key="t-todo">Challenges</span>
+
+            </a>
+        </li>
+        <li>
+            <a href="settings.php">
+                <i class="mdi mdi-cog-outline icon nav-icon"></i>
+                <span class=" menu-item" data-key="t-filemanager">Settings</span>
+            </a>
+        </li>
+
+
+    </ul>
+</div>
